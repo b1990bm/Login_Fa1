6090480d04baa60c4e7a29738677850da4516801
Behnam <b1990bm@gmail.com>
2022-05-05 17:52:47 +0430

    Start Project

---
+++ src/components/Register.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Register = () => {
+    return (
+        <div>
+            <h1>ثبت نام</h1>
+            <div>
+                
+            </div>
+        </div>
+    );
+};
+
+export default Register;
