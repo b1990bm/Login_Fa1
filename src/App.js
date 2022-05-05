429adeddebad35f75b1cf1e7e718348614b8b1fb
Behnam <b1990bm@gmail.com>
2022-05-05 21:10:11 +0430

    localstorage setItem

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Register from './components/Register';
 import Login from "./components/Login";
-import {Route,Routes,Navigator} from 'react-router-dom';
+import {Route,Routes} from 'react-router-dom';
 
 const App = () => {
   return (
