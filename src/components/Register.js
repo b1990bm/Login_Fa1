4ea4700e184d959968145771c51ed1b1d23d5d91
Behnam <b1990bm@gmail.com>
2022-05-05 21:10:41 +0430

    localstorage setItem

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,25 +1,51 @@
 import React from 'react';
 import "../Styles/style.scss";
 import {Link} from 'react-router-dom';
+import { useState } from 'react';
 const Register = () => {
+    const [name,setName]= useState("");
+    const [email,setEmail]= useState("");
+    const [password,setPassword]= useState("");
+       
+    
+
+    const sumbitHandeler=(e)=>{
+        e.preventDefault();
+
+            localStorage.setItem("name",JSON.stringify(name));
+            localStorage.setItem("email",JSON.stringify(email));
+            localStorage.setItem("password",JSON.stringify(password));
+
+
+
+    }
+
+    
     return (
         <div>
-            <form className="form">
+            <form className="form" onSubmit={sumbitHandeler}>
             <h1 className="headerName">ثبت نام</h1>
             <div className="form-group">
                 <label>نام</label>
-                <input type="text" className="form-control" 
-                                    placeholder="نام خود را وارد کنید" />
+                <input type="text" name="name" className="form-control" 
+                                    placeholder="نام خود را وارد کنید" 
+                                    onChange={(e)=>setName(e.target.value)}                    
+                                    />
+
             </div>
             <div className="form-group">
                 <label>ایمیل</label>
-                <input type="email" className="form-control" 
-                                    placeholder="ایمیل خود را وارد کنید" />
+                <input type="email" name="email" className="form-control" 
+                                    placeholder="ایمیل خود را وارد کنید" 
+                                    onChange={(e)=>setEmail(e.target.value)}  
+                                    />
             </div>
             <div className="form-group">
                 <label>پسورد</label>
-                <input type="text" className="form-control" 
-                                    placeholder="پسورد خود را وارد کنید" />
+                <input type="text" name="password" className="form-control" 
+                                    placeholder="پسورد خود را وارد کنید" 
+                                    onChange={(e)=>setPassword(e.target.value)}  
+                                    />
             </div>
             <div className="form-group">
                 <label>شماره تماس</label>
