1379fe6d0dc6a4fd59f9f447a5a74386e343d290
Behnam <b1990bm@gmail.com>
2022-05-06 18:28:24 +0430

    Create Navbar

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from 'react';
 import Register from './components/Register';
 import Login from "./components/Login";
 import {Route,Routes} from 'react-router-dom';
+import Navbar from './components/Navbar';
 
 const App = () => {
   return (
     <div>
+      <Navbar />
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
---
+++ src/components/Navbar.js
@@ -0,0 +1,39 @@
+import React,{useState} from 'react';
+import { Link } from 'react-router-dom';
+import Logo from "../image/logo.svg";
+import Search from "../image/search.svg";
+import login from "../image/login.svg";
+const Navbar = () => {
+    const [search,setSearch]=useState(false);
+    return (
+        <div className="navContainer">
+
+            <div className="rightBar">
+                <img src={Logo} alt="Logo" />
+                <div className="menuBar">
+                    <Link className="linkmenu" to="" > محصولات</Link>
+                    <Link className="linkmenu" to="" > وبلاگ</Link>
+                    <Link className="linkmenu" to="" > تماس با ما</Link>
+                </div>
+            </div>
+
+            <div className="leftBar">
+
+                {
+                    search===true ? 
+                    <input type="text" className="searchBar" placeholder="عبارت خود رو وارد کنید"/> :
+                    <img src={Search} onClick={()=>setSearch(true)} alt="search" />
+
+                }
+                  
+                <div className="buttonBar">
+                    <Link className='btnlogin' to="/login"><img src={login} alt="" /></Link>
+                </div>
+
+            </div>
+            
+        </div>
+    );
+};
+
+export default Navbar;
